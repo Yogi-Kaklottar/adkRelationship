dcc247e27f83888b4d65f75d0da7614d43e088ee
Yogi-Kaklottar <kaklottary@gmail.com>
2020-01-02 16:57:08 +0530

    action method for student example

---
+++ axelor-test-hr/src/main/java/com/app/hr/web/StudentController.java
@@ -0,0 +1,41 @@
+package com.app.hr.web;
+
+import com.axelor.hr.db.Student;
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+import com.ibm.icu.math.BigDecimal;
+
+public class StudentController {
+
+  public void setResult(ActionRequest request, ActionResponse response) {
+    Student s = request.getContext().asType(Student.class);
+    s.setTotal(s.getMarks1()+s.getMarks2()+s.getMarks3());
+    java.math.BigDecimal n;
+    Double no=(double) (s.getTotal()/3);    
+    n=new java.math.BigDecimal(no.toString());
+    s.setPersantage(n);
+    if(no>=70)
+    {
+    	s.setGrade("Distiction");
+    }
+    else if(70<no && 60>=no)
+    {
+    	s.setGrade("firstClass");
+    }
+    else if(60<no && 50>=no)
+    {
+    	s.setGrade("second class");
+    }
+    else if(60<no && 50>=no)
+    {
+    	s.setGrade("passClass");
+    }
+    else
+    {
+    	s.setGrade("fail");
+    }
+        
+    System.out.println(s.toString());
+    response.setValues(s);
+  }
+}
